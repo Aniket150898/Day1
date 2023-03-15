2f4e089c23898fbeb4ba646251ed366352888186
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-15 18:12:00 +0530

    [Aniket]- Perfrom Object/Class in c#

---
+++ OopsConcepts/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts
+{
+    internal class Class1
+    {
+        public void Animal()
+        {
+
+            Console.WriteLine("Animal is Eating");
+
+        }
+        public static void Animal2()
+        {
+
+            Console.WriteLine("Animal is Eating grass with drinking water");
+
+        }
+    }
+}
--- OopsConcepts/Program.cs
+++ OopsConcepts/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to OOPS Concepts");
+            Console.WriteLine("Hello I'm Object here");
+            Class1 class1 = new Class1(); //Object create just becuse in Class1 we have declare instance Method.
+            class1.Animal();
+            Class1.Animal2(); //Here we are not creating object directly calling class with static Method which we have declare
         }
     }
 }
