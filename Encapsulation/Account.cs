a2df35bb3b5c1501e26f30a62620eab30771459b
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-15 18:52:09 +0530

    [Aniket]- Perfrom Encapsulation in c#

---
+++ Encapsulation/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Account
+    {
+        int accountsBalance = 1000;
+        public void SetBalance(int balance)
+        {
+            this.accountsBalance = balance;
+        }
+        public void GetBalance()
+        {
+            Console.WriteLine("Your Balance is " + accountsBalance);
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Principal;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Account myaccount = new Account();
+            myaccount.SetBalance(100);
+            myaccount.GetBalance();
+        }
+    }
+}
