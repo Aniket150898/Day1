f54a62d2ef6b384ea121e356b0c8687d8f83474b
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-15 19:16:51 +0530

    [Aniket]- Perfrom Abstraction in c#

---
+++ Abstraction/AbstractAnimal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    abstract class AbstractAnimal
+    {
+        // Abstract method does not have a body
+        public abstract void animalSound();
+        // Regular method
+        public void sleep()
+        {
+            Console.WriteLine("Animal is is Sleeping");
+        }
+    }
+
+   
+    class Pig : AbstractAnimal
+    {
+        public override void animalSound()
+        {
+            
+            Console.WriteLine("The pig says:- Weeeeee!!!!1");
+        }
+    }
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig(); // Create a Pig object
+            myPig.animalSound();  // Call the abstract method
+            myPig.sleep();       // Call the regular method
+        }
+    }
+}
