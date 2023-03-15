2cb9505c87fecf9165734e324bdb76a3f971ac07
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-15 18:25:08 +0530

    [Aniket]- Perfrom Inheritance in c#

---
+++ Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Performing Inheritance");
+            Dog dog = new Dog();
+            dog.Eat();
+        }
+    }
+}
