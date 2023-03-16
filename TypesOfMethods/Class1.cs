0848237b53c3923a80e72a6f459a01e8432a1d2a
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-16 10:21:20 +0530

    [Aniket]- Perfrom Types of Methods in c#

---
+++ TypesOfMethods/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesOfMethods
+{
+    internal class Class1
+    {
+        // Instance Method
+        public void Animal()
+        {
+
+            Console.WriteLine("Animal is Eating");
+
+        }
+        //static Method
+        public static void Animal2()
+        {
+
+            Console.WriteLine("Animal is Eating grass with drinking water");
+
+        }
+    }
+}
---
+++ TypesOfMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TypesOfMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Types Of Methods in C#");
+            Class1 class1 = new Class1(); //Object create just because in Class1 we have declare instance Method.
+            class1.Animal();
+            Class1.Animal2(); //Here we are not creating object directly calling class with static Method which we have declare
+        }
+    }
+}
