fedbbb96cd4bd4df057e04fb52940da422039965
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-16 10:14:58 +0530

    [Aniket]- Perfrom Types of varibales in c#

---
+++ Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Variables Types");
+            Variables variables = new Variables();
+            variables.EmployeeAge();//Local
+            Variables.Salary = 46000;//Static
+            Console.WriteLine("Employee Salary = " + Variables.Salary);
+            Console.WriteLine("Employee Salary = " + Variables.Monthlysalary);//Const variable
+            Console.WriteLine("Employee LastName;" + variables.LastName);//readomnly
+            variables.EmployeeName = "Aniket";//Instance 
+            variables.EmployeeId = 1;
+            Console.WriteLine("Employee Name= " + variables.EmployeeName + " \nEmployee ID; " + variables.EmployeeId);
+            Variables variables1 = new Variables();
+            variables1.EmployeeName = "Sky";//Instance Second Object
+            variables1.EmployeeId = 2;
+            Console.WriteLine("Employee Name= " + variables1.EmployeeName + " \nEmployee ID; " + variables1.EmployeeId);
+        }
+    }
+}
---
+++ Variables/Variables.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Variables
+    {
+        public static double Salary;//static variables
+        public static string Name = "Aniket";
+        public string EmployeeName;//Instance or Non Static Variables
+        public int EmployeeId;
+        public const float Monthlysalary = 20000;
+        public readonly string LastName;//ReadOnly
+        public Variables()
+        {
+            
+            this.LastName = "Gamechanger";
+        }
+        public void EmployeeAge()
+        {
+            int age = 20;//Local Variables
+            Console.WriteLine(" Marks of Student :" + age);
+
+        }
+    }
+}
